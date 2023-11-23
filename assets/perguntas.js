24e88946c5a9358ef8264916815c47be54286d54
mairlan <mairlan.mengao@gmail.com>
2023-11-23 18:07:19 -0300

    novo quiz

---
+++ assets/perguntas.js
@@ -0,0 +1,55 @@
+const perguntas = [
+    {
+        question : "A candidíase oral, ou candidose, é uma infecção fúngica. Exibe uma variedade de padrões clínicos, e a mais bem reconhecida é Candidíase pseudomembranosa (ou sapinho). As características da apresentação dessa doença são:", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        answers  : [
+            { text: "A) vesículas avermelhadas, localizadas na região dos lábios e palato.", correct : false},
+            { text: "B) placas brancas não destacáveis, localizadas na mucosa jugal anterior.", correct : false},
+            { text: "C) lesões fissuradas avermelhadas, localizadas na região de comissura labial.", correct : false},
+            { text: "D) áreas atróficas e vermelhas, localizadas na região posterior do dorso lingual", correct : false},
+            { text: "E) placas brancas, cremosas, destacáveis, localizadas na mucosa jugal, língua e palato.", correct : true},
+        ]
+    },
+    {
+        question : "Qual medicamento é o tratamento para candidíase?", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        answers  : [
+            { text: "A) Anti-inflamatório", correct : false},
+            { text: "B) Antifúngico", correct : true},
+            { text: "C) Sedativo", correct : false},
+            { text: "D) Analgésico", correct : false},
+        ]
+    },
+    {
+        question : "Sobre os sintomas da candidíase oral, marque verdadeiro ou falso:", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        answers  : [
+            { text: "Mau hálito ou secura", correct : true},
+            { text: "Não tem sintomas visíveis", correct : false},
+            { text: "A candidíase, mais conhecida como “sapinho” provoca lesões brancas na língua ou na parte interna das bochechas.", correct : true},
+            { text: "Dores e amolecimento nos dentes, e na gengiva ", correct : false},
+            { text: "Lesões avermelhadas como aftas", correct : true},
+            { text: "Anormalidade no paladar, dificuldade em engolir, lesão ou vermelhidão", correct : true},
+        ]
+    },
+    {
+        question : "A candidíase oral pode ser diagnóstica por quais profissionais?", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        answers  : [
+            { text: "A) Clínico geral e farmacêutico", correct : false},
+            { text: "B) Somente um farmacêutico", correct : true},
+            { text: "C) Clínico geral, dentista ou pediatra no caso de crianças", correct : true},
+            { text: "D) Apenas o Clínico geral", correct : false},
+        ]
+    },
+    {
+        question : "As infecções da candidíase oral podem acontecer em grupos específicos. Portanto, pode-se afirmar que existe um grande leque de fatores predisponentes e que podem, até mesmo, acontecer mutuamente. Dessa forma, quais pacientes dos seguintes grupos estão mais vulneráveis? Assinale Verdadeiro ou Falso", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        answers  : [
+            { text: "Pessoas com HIV/AIDS", correct : true},
+            { text: "Imaturidade imunológica da infância", correct : true},
+            { text: "Pessoas com uma saúde bucal em dias", correct : false},
+            { text: "Pessoas com higiene precária", correct : true},
+        ]
+    },
+]
---
+++ assets/script.js
@@ -0,0 +1,105 @@
+// import perguntas from 'perguntas';
+// perguntas
+
+const perguntas = [
+    {
+        question : "A candidíase oral, ou candidose, é uma infecção fúngica. Exibe uma variedade de padrões clínicos, e a mais bem reconhecida é Candidíase pseudomembranosa (ou sapinho). As características da apresentação dessa doença são:", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        VF    : false,
+        answers  : [
+            { text: "A) vesículas avermelhadas, localizadas na região dos lábios e palato.", correct : false},
+            { text: "B) placas brancas não destacáveis, localizadas na mucosa jugal anterior.", correct : false},
+            { text: "C) lesões fissuradas avermelhadas, localizadas na região de comissura labial.", correct : false},
+            { text: "D) áreas atróficas e vermelhas, localizadas na região posterior do dorso lingual", correct : false},
+            { text: "E) placas brancas, cremosas, destacáveis, localizadas na mucosa jugal, língua e palato.", correct : true},
+        ]
+    },
+    {
+        question : "Qual medicamento é o tratamento para candidíase?", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        VF    : false,
+        answers  : [
+            { text: "A) Anti-inflamatório", correct : false},
+            { text: "B) Antifúngico", correct : true},
+            { text: "C) Sedativo", correct : false},
+            { text: "D) Analgésico", correct : false},
+        ]
+    },
+    {
+        question : "Sobre os sintomas da candidíase oral, marque verdadeiro ou falso:", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        VF    : true,
+        answers  : [
+            { text: "Mau hálito ou secura", correct : true},
+            { text: "Não tem sintomas visíveis", correct : false},
+            { text: "A candidíase, mais conhecida como “sapinho” provoca lesões brancas na língua ou na parte interna das bochechas.", correct : true},
+            { text: "Dores e amolecimento nos dentes, e na gengiva ", correct : false},
+            { text: "Lesões avermelhadas como aftas", correct : true},
+            { text: "Anormalidade no paladar, dificuldade em engolir, lesão ou vermelhidão", correct : true},
+        ]
+    },
+    {
+        question : "A candidíase oral pode ser diagnóstica por quais profissionais?", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        VF    : false,
+        answers  : [
+            { text: "A) Clínico geral e farmacêutico", correct : false},
+            { text: "B) Somente um farmacêutico", correct : true},
+            { text: "C) Clínico geral, dentista ou pediatra no caso de crianças", correct : true},
+            { text: "D) Apenas o Clínico geral", correct : false},
+        ]
+    },
+    {
+        question : "As infecções da candidíase oral podem acontecer em grupos específicos. Portanto, pode-se afirmar que existe um grande leque de fatores predisponentes e que podem, até mesmo, acontecer mutuamente. Dessa forma, quais pacientes dos seguintes grupos estão mais vulneráveis? Assinale Verdadeiro ou Falso", 
+        hint    : "Este teste avalia como o corpo processa a glicose após a ingestão de uma quantidade padronizada de açúcar.",
+        VF    : true,
+        answers  : [
+            { text: "Pessoas com HIV/AIDS", correct : true},
+            { text: "Imaturidade imunológica da infância", correct : true},
+            { text: "Pessoas com uma saúde bucal em dias", correct : false},
+            { text: "Pessoas com higiene precária", correct : true},
+        ]
+    },
+];
+
+
+
+let PerguntaAtual = 0;
+let pontos = 0;
+let questionToAsk = 0;
+
+const IniciarQuiz = document.getElementById("IniciaQuiz");
+const btnIniciar = document.getElementById("iniciar");
+const LocalPergunta = document.getElementById("LocalPergunta");
+const pergunta = document.getElementById("pergunta");
+const resposta = document.getElementById("respostas");
+
+function quiz(){
+
+    //let pergunta = perguntas[PerguntaAtual];
+
+    if (PerguntaAtual < perguntas.length){
+        IniciarQuiz.style.display = "none";
+        LocalPergunta.style.display = "flex";
+        pergunta.textContent = PerguntaAtual+1 + ") " + perguntas[PerguntaAtual].question;
+    }
+    if(perguntas.VF == false){
+        perguntas[PerguntaAtual].answers.forEach(answer => {
+            // variavel que cria um botão dentro do HTML
+            const button = document.createElement("button")
+            //colocar as Alternativas nos botões
+            button.innerHTML = answer.text;
+            //define as classes dos botões como "btn"
+            button.classList.add("resp");
+            // chama a variavel para criar com filho da DIV "answer-buttons"
+            resposta.appendChild(button);
+            // verifica se clicou na resposta correta
+            if(answer.correct){
+                // se sim, define o valor como correto
+                button.dataset.correct = answer.correct;
+            }
+        })
+    }else {
+        // perguntas de verdadeiro ou falso
+    }
+}
